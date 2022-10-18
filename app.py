edd87b3e2158744010ed797033f907927ed7fe82
callumrowston <callum.rowsto@gmail.com>
2022-10-18 12:54:40 +1100

    Added users and bcrypt

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import email
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config ['JSON_SORT_KEYS'] = False
@@ -12,7 +14,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:passwo
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
 
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'is_admin')
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -42,6 +57,19 @@ def drop_db():
 
 @app.cli.command('seed')
 def seed_db():
+    users = [
+        User(
+            email='admin@spam.com',
+            password=bcrypt.generate_password_hash('eggs').decode('utf-8'),
+            is_admin=True
+        ),
+        User(
+            name='John CLeese',
+            email='someone@spam.com',
+            password=bcrypt.generate_password_hash('12345').decode('utf-8')
+        )
+    ]
+
     cards = [
         Card(
             title = 'Start the project',
@@ -74,6 +102,7 @@ def seed_db():
     ]
 
     db.session.add_all(cards)
+    db.session.add_all(users)
     db.session.commit()
     print('Tables Seeded')
 
@@ -85,7 +114,6 @@ def all_cards():
     stmt = db.select(Card).order_by(Card.priority.desc(), Card.title)
     cards = db.session.scalars(stmt)
     return CardSchema(many=True).dump(cards)
-
     # for card in cards:
     #     print(card.title, card.priority)
     # print(cards)
